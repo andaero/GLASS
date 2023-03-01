aea26d6cf48f4734f6b1608bfa32405149bf1a5a
axema <axeman775@gmail.com>
2023-03-01 10:48:35 -0500

    Added column name to parser

--- prediction_eval.py
+++ prediction_eval.py
@@ -6,11 +6,12 @@ parser = argparse.ArgumentParser(description="Prediction Evaluator")
 parser.add_argument("--path", type=str,
     default='predictions/01_21-59/test_predictions_mp_onehot_MP_8_12_100_.csv')
 parser.add_argument("--threshold", type=float, default=100)
+parser.add_argument("--col", type=str, default='prediction')
 
 options = parser.parse_args()
 print(options.path)
 
 df = pd.read_csv(options.path)
 
-df_threshold = df[df['prediction'] > options.threshold]
+df_threshold = df[df[options.col] > options.threshold]
 print(df_threshold)
