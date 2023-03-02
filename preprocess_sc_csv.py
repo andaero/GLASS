9ab529ee7b3a0347829e8b063b13f465892a6b36
axema <axeman775@gmail.com>
2023-03-01 20:58:18 -0500

    Updated targets with pretty formula

--- preprocess_sc_csv.py
+++ preprocess_sc_csv.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import os
 
-df = pd.read_csv("3DSC_MP.csv", skiprows=1, usecols=["cif","tc"])[["cif","tc"]]
+df = pd.read_csv("3DSC_MP.csv", skiprows=1, usecols=["cif","tc", "formula_sc"])[["cif","tc","formula_sc"]]
 print(df)
 df["id"] = df.apply(lambda x: os.path.basename(x["cif"]).rsplit('.',1)[0], axis=1)
 print(df)
-df.to_csv("database/targets_SC.csv", columns=['id', 'tc'], index=False)
+df = df.rename(columns={"formula_sc": "pretty_formula"})
+df.to_csv("database/targets_SC_v2.csv", columns=['id', 'tc','pretty_formula'], index=False)
